9bd85d6de765c7685074111ef62f2d066c71c0f9
huykhoa11 <69473375+huykhoa11@users.noreply.github.com>
2021-12-24 19:08:06 +0900

    Update count time and desc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
 	def favorite_text
 		return @favorite_exists? "UnFavorite" : "Favorite"  #True -> "UnFavorite",  False -> "Favorite"
 	end
+	
 
 	helper_method :favorite_text
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -38,7 +38,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to "/posts", notice: "Post was successfully created." }
+        format.html { redirect_to "/posts##{@post.id}", notice: "Post was successfully created." }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new, status: :unprocessable_entity }
