03c3bf86b3167f3a3964e71395619ddf81a86de9
huykhoa11 <69473375+huykhoa11@users.noreply.github.com>
2022-01-06 19:30:55 +0900

    Add avatar function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,19 @@
 class ApplicationController < ActionController::Base
+	before_action :configure_permitted_parameters,
+					if: :devise_controller?
+
 	def favorite_text
 		return @favorite_exists? "UnFavorite" : "Favorite"  #True -> "UnFavorite",  False -> "Favorite"
 	end
-	
 
 	helper_method :favorite_text
+
+	protected
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
+		devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
+	end
+	
+
+	
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,36 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+	# Include default devise modules. Others available are:
+	# :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+	devise :database_authenticatable, :registerable,
+			:recoverable, :rememberable, :validatable
 
-         has_many :posts
+	has_many :posts
+	has_one_attached :avatar
+	after_commit :add_default_avatar, on: %i[create update]
+
+	def avatar_thumbnail
+		if avatar.attached?
+			#avatar.variant(resize: '50x50!').processed
+		else
+			"/default_profile.png"
+		end
+	end
+
+
+	private
+	def add_default_avatar
+		unless avatar.attached?
+			avatar.attach(
+				io: File.open(
+					Rails.root.join(
+						'app', 'assets', 'images', 'default_profile.png'
+					)
+				),
+				filename: "default_profile.png",
+				content_type: 'image/jpg'
+			)
+		end
+	end
+
+
 end
