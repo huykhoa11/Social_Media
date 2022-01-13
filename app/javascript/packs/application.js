305c85f173ba218fb0d883dd4f9c8dfb7e788d2a
huykhoa11 <69473375+huykhoa11@users.noreply.github.com>
2022-01-13 16:24:49 +0900

    validate images presence

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,6 +20,50 @@
 // 	});
 // });
 
+$(function(){
+
+	var intervalId;
+	setTimer();
+
+	function setTimer() {
+		intervalId = setInterval(autoClick, 2000);
+	}
+
+	function autoClick() {
+		$(".slide").children("a.next").click();
+	}
+
+	function changeImages($click) {
+		//console.log($click)
+		var $current = $click.siblings(".container").children(".current");
+		var $new;
+		var findSelector = "";
+
+		if($click.hasClass("next")) {
+			$new = $current.next();
+			findSelector = ":first-child";
+		}
+		else {
+			$new = $current.prev();
+			findSelector = ":last-child";
+		}
+
+		if($new.length == 0) {
+			$new = $current.siblings(findSelector);
+		}
+		$current.removeClass("current");
+		$new.addClass("current");
+		setTimer();
+	}
+
+	$(".slide").on("click", "> a", function(event){
+		event.preventDefault();
+		clearInterval(intervalId);
+		changeImages($(this));
+	})
+
+});
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,13 @@ class Post < ApplicationRecord
     validates :title, presence: true
     belongs_to :user
     has_many_attached :images
+    validate :image_type
+
+    private
+    def image_type
+    	if images.attached? == false
+    		errors.add(:images, "are missings")
+    	end
+    end
+
 end
