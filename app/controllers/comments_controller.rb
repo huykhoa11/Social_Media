56bbc621381a8948d4dd96e74ee142793fc79df2
huykhoa11 <69473375+huykhoa11@users.noreply.github.com>
2022-04-15 00:44:31 +0900

    fix ajax bug

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,9 +8,12 @@ class CommentsController < ApplicationController
     @comment.user_id = current_user.id
     @comment.save
     #redirect_to post_path(@post)
-    redirect_to "/posts##{@post.id}"
+    #redirect_to "/posts##{@post.id}"
     #redirect_to :back
     #redirect_back(fallback_location: root_path(anchor: "form-create"))
+    respond_to do |format|
+      format.js
+    end
 
   end
   
@@ -19,9 +22,12 @@ class CommentsController < ApplicationController
       @comment = @post.comments.find(params[:id])
       @comment.destroy
       #redirect_to post_path(@post)
-      redirect_to "/posts##{@post.id}"
+      #redirect_to "/posts##{@post.id}"
       #redirect_to :back
       #redirect_back(fallback_location: root_path(anchor: 'form-create'))
+      respond_to do |format|
+        format.js
+      end
   end
 
   private
